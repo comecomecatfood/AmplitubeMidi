e0aa52e75fb0a50272bf266b49773d7b23e807c0
Yuta Watanabe <yuta.watanabe@soliton.co.jp>
2020-10-26 23:00:55 +0000

    Button3に対応

--- sample.py
+++ sample.py
@@ -11,12 +11,13 @@ midiout, port_name = open_midioutput(1)
 
 class FootController(object):
     def __init__(self):
+        self.power_led = LED(6)
         self.ts = {
             'led': LED(17),
             # value 100 is off
             'value': 100,
         }
-        self.ts_button = Button(2)
+        self.ts_button = Button(13)
         self.ts_button.when_pressed = self.ts_change
 
         self.delay = {
@@ -24,8 +25,18 @@ class FootController(object):
             # value 100 is off
             'value': 100,
         }
-        self.delay_button = Button(3)
+        self.delay_button = Button(19)
         self.delay_button.when_pressed = self.delay_change
+
+        self.num3 = {
+            'led': LED(22),
+            # value 100 is off
+            'value': 100,
+        }
+        self.num3_button = Button(26)
+        self.num3_button.when_pressed = self.num3_change
+
+        self.power_led.on()
 
     def ts_change(self):
         if self.ts['value'] == 100:
@@ -50,6 +61,19 @@ class FootController(object):
             print('Delay OFF')
         controller_change = [CONTROLLER_CHANGE, 0x51, self.delay['value']]
         midiout.send_message(controller_change)
+
+    def num3_change(self):
+        if self.num3['value'] == 100:
+            self.num3['led'].on()
+            self.num3['value'] = 0
+            print('num3 ON')
+        else:
+            self.num3['led'].off()
+            self.num3['value'] = 100
+            print('Num3 OFF')
+        controller_change = [CONTROLLER_CHANGE, 0x52, self.num3['value']]
+        midiout.send_message(controller_change)
+
 
 foot_controller = FootController()
 
